b354904412f0579f2858a5817c510495da5ae163
jgluiggi lcc <jgluiggi@protonmail.com>
2024-09-12 17:31:33 -0300

    feat: printando arquivos no /tmp/dataset

--- main.go
+++ main.go
@@ -2,9 +2,40 @@ package main
 
 import (
     "log"
-	"google.golang.org/grpc"
+    "os"
+	//"google.golang.org/grpc"
+)
+
+const (
+    dir = "/tmp/dataset"
 )
 
 func main() {
+    shareFiles()
+}
 
+func searchFiles() {
+}
+
+func shareFiles() {
+    d, err := os.Open(dir)
+    if err != nil {
+        log.Fatalf("Erro ao abrir diretório: ", err)
+        return
+    }
+    defer d.Close()
+
+    files, err := d.Readdir(-1)
+    if err != nil {
+        log.Fatalf("Erro ao abrir diretório: ", err)
+        return
+    }
+
+    for _, file := range files {
+        if file.IsDir() {
+            log.Printf("Diretório: %s\n", file.Name())
+        } else {
+            log.Printf("Arquivo: %s\n", file.Name())
+        }
+    }
 }
